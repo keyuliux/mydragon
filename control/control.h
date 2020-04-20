5a7de63a4155a6935b3b1180cdfaeec899f85ab1
keyuliux <liu_keyu@hotmail.com>
2020-04-20 14:25:35 +0800

    add pass struct define and related head files

---
+++ control/control.h
@@ -0,0 +1,9 @@
+#ifndef CONTROL_H
+#define CONTROL_H
+
+
+typedef enum {VERBOSE_SILENT, 
+              VERBOSE_PASS, 
+              VERBOSE_SUBPASS, 
+              VERBOSE_DETAIL} Verbose_t;
+#endif
