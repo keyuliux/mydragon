142bcc1075411e1220ff3745f15dee2a0b67dcd5
keyuliux <liu_keyu@hotmail.com>
2020-04-20 13:45:30 +0800

    add main logic

---
+++ main/main-main.c
@@ -0,0 +1,25 @@
+#include "main-main.h"
+
+int Main_main (int argc, char **argv)
+{
+    // We are using the Boehm garbage collect, which
+    // requires the initialization of the memory 
+    // management component.
+    // This code does this. 
+    // Read online info. here:
+    //   http://www.hpl.hp.com/personal/Hans_Boehm/gc/
+
+    //to test the libraries
+
+    // If only type "dragon", then print some info and exit.
+    
+    // else cook commandline arguments. 
+    // Return a list of file names.
+    // E.g.: for the input
+    //   dragon -expert true -verbose 2 a.c
+    // will return ["a.c"].
+    
+    //create dragon pass
+
+    return 0;
+}
